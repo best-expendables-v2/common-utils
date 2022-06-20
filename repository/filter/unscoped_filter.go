678a6be06ad24e5d1c5296559f34bd8f1557ca18
quangpn <quangpn88@gmail.com>
2022-06-20 21:31:49 +0700

    support preload unscoped

--- repository/filter/unscoped_filter.go
+++ repository/filter/unscoped_filter.go
@@ -6,13 +6,25 @@ import (
 )
 
 var isUnscopedKey = "is_unscoped"
+var isPreloadUnscopedKey = "is_preload_unscoped"
 
 func SetUnscoped(ctx context.Context) context.Context {
 	return context.WithValue(ctx, isUnscopedKey, true)
 }
 
+func SetPreloadUnscoped(ctx context.Context) context.Context {
+	return context.WithValue(ctx, isPreloadUnscopedKey, true)
+}
+
 func GetUnscoped(ctx context.Context) bool {
 	if v := ctx.Value(isUnscopedKey); v != nil {
+		return v.(bool)
+	}
+	return false
+}
+
+func GetPreloadUnscoped(ctx context.Context) bool {
+	if v := ctx.Value(isPreloadUnscopedKey); v != nil {
 		return v.(bool)
 	}
 	return false
@@ -23,6 +35,9 @@ func DBUnscopedFilter(next http.Handler) http.Handler {
 		values := r.URL.Query()
 		if values.Get("unscoped") == "true" {
 			r = r.WithContext(SetUnscoped(r.Context()))
+		}
+		if values.Get("preload_unscoped") == "true" {
+			r = r.WithContext(SetPreloadUnscoped(r.Context()))
 		}
 		next.ServeHTTP(w, r)
 	})
